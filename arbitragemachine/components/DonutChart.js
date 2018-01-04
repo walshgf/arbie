ea7dc20557edb3905f5cb8b993d8a1c397f47e9c
atiffany <abby.tiffany@gmail.com>
2018-01-04 12:00:53 -0800

    small reorg of files

---
+++ arbitragemachine/components/DonutChart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class DonutChart {
+propTypes: {
+    value: React.PropTypes.number,
+    valueLabel: React.PropTypes.string,
+    size: React.PropTypes.number,
+    strokeWidth: React.PropTypes.number
+},
+
+}
