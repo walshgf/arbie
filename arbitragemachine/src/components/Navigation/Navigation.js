afa3bbd7566343061304c377146188cca0a24bb7
atiffany <abby.tiffany@gmail.com>
2018-01-04 16:19:55 -0800

    links operational

--- arbitragemachine/src/components/Navigation/Navigation.js
+++ arbitragemachine/src/components/Navigation/Navigation.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import {
+    BrowserRouter,
+    Link
+    } from 'react-browser-router';
 import './Navigation.css';
 
 export default function Navigation() {
     return (
-	<div id="header-container">
-    <header class="wrapper clearfix">
-        <h1 id="title">Arbie - We Have the Deets!</h1>
-        <nav>
-            <ul>
-                <li><a href="#">Arbie</a></li>
-                <li><a href="#">Chart</a></li>
-                <li><a href="#">Team</a></li>
-            </ul>
-        </nav>
-    </header>
-</div>
+        <BrowserRouter>
+            <div id="header-container">
+            <header class="wrapper clearfix">
+                <h1 id="title">Arbie - We Have the Deets!</h1>
+                <nav>
+                    <ul>
+                        <li><Link to="/home">Arbie</Link></li>
+                        <li><Link to="/chart">Chart</Link></li>
+                        <li><Link to="/">Team</Link></li>
+                    </ul>
+                </nav>
+            </header>
+            </div>
+        </BrowserRouter>
     );
 }
