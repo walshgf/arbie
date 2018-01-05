119fb0fff9703418333e6181d3d5e5c4b1ef61cd
kairat abylkasymov <kairat.ninety@gmail.com>
2018-01-04 23:13:14 -0500

    backend

---
+++ backend/server/README.md
@@ -0,0 +1,5 @@
+## For the backend 
+
+- Run `npm install` from root direcotry 
+- install `nodemon` via `npm install -g` 
+- then run `nodemon app.js` from root directory
---
+++ backend/server/routes/index.js
@@ -0,0 +1,43 @@
+const getGdax = require('../controllers/GDAX/gdax');
+const getPoloniex = require('../controllers/POLONIEX/poloniex');
+const getGemini = require('../controllers/GEMINI/gemini');
+
+module.exports = (app) => {
+
+    app
+        .route('/get-productsgdax')
+        .get(getGdax.getProducts);
+    
+    app 
+        .route('/get-producttickergdax')
+        .get(getGdax.getProductTicker);
+
+    app
+        .route('/get-producttradebtcgdax')
+        .get(getGdax.getProductTradeBTC);
+
+    app
+        .route('/get-producttradeethgdax')
+        .get(getGdax.getProductTradeETH);
+
+    app
+        .route('/get-test')
+        .get(getGdax.getProductHistoricRates);
+
+    app
+        .route('/get-poloniexETH')
+        .get(getPoloniex.returnTickerETH);
+
+    app
+        .route('/get-poloniexBTC')
+        .get(getPoloniex.returnTickerBTC);
+    
+    app
+        .route('/get-geminiBTC')
+        .get(getGemini.getTickerBTC);
+
+    app
+        .route('/get-geminiETH')
+        .get(getGemini.getTickerETH);
+
+}
