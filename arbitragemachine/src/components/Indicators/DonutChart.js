adc0a1b2b7ca7d429543243f330b44f3bcb244e2
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-01-06 01:26:59 -0500

    App Functional

--- arbitragemachine/src/components/Indicators/DonutChart.js
+++ arbitragemachine/src/components/Indicators/DonutChart.js
@@ -4,14 +4,25 @@ import './DonutChart.css';
 class DonutChart extends React.Component{
     constructor(props) {
         super(props);
+        console.log(this.props.value);
         this.state = {
+            value: null,
+            lowSeller: null,
+            highBuyer:null,
+            valueLabel: null,
+            size: null,
+            strokeWidth: null,
+        };
+    }
+    componentDidMount(){
+        this.setState({
             value: this.props.value,
             lowSeller: this.props.lowSeller,
             highBuyer: this.props.highBuyer,
             valueLabel: 'Completed',
             size: 116,
             strokeWidth: 26,
-        };
+        });
     }
     render() {
         const halfSize = (this.state.size * 0.5);
@@ -26,21 +37,24 @@ class DonutChart extends React.Component{
 
         const decisionToTrade = this.state.value >= 5 ? 'Trade' : 'Hold';
 
+
         return (
             <div>
                 <svg width={this.state.size} height={this.state.size} className ="donutchart">
                 <circle r={radius} cx={halfSize} cy={halfSize} transform={rotateVal} style={trackStyle} className="donutchart--track"/>
                     <circle r={radius} cx={halfSize} cy={halfSize} transform={rotateVal} style={indicatorStyle} className={this.state.value >= 5 ? "donutchart--indicator--green" : "donutchart--indicator--red"}/>
                     <text className="donutchart--text" x={halfSize} y={halfSize} style={{textAnchor:'middle'}} >
-                        <tspan className="donutchart--text_val">{this.props.value}</tspan>
+                        <tspan className="donutchart--text_val">{(this.props.value).toFixed(0)}</tspan>
                         <tspan className="donutchart--text_percent">%</tspan>
                         <tspan className="donutchart--text_label" x={halfSize} y={halfSize+10}>{this.props.valueLabel}</tspan>
                     </text>
                 </svg>
                 <div>
                     <p><b>We Recommend You {decisionToTrade}</b></p>
-                    <p>Arbitrage Percentage: {this.state.value}%</p>
-                    <div id ={this.state.value >= 5 ? 'data' : 'hidden'}>
+                    <p>Arbitrage Percentage: {(this.props.value).toFixed(2)}%</p>
+                    <div>HP:{(this.props.highPrice).toFixed(2)} LP:{(this.props.lowPrice).toFixed(2)}</div>
+                    <div>${(this.props.highPrice - this.props.lowPrice).toFixed(2)} Profit </div>
+                    <div id ={this.props.value >= 5 ? 'data' : 'hidden'}>
                     <p>Buy from: {this.state.lowSeller}</p>
                     <p>Sell to: {this.state.highBuyer}</p>
                     </div>
--- arbitragemachine/src/components/Indicators/Indicators.js
+++ arbitragemachine/src/components/Indicators/Indicators.js
@@ -20,17 +20,24 @@ class Indicators extends React.Component {
         this.bitcoinArbitrageValue = null;
         this.bitcoinLowSeller = null;
         this.bitcoinHighBuyer = null;
+        this.bitcoinHighPrice = null;
+        this.bitcoinLowPrice = null;
         this.ethereumArbitrageValue = null;
         this.ethereumLowSeller = null;
         this.ethereumHighBuyer = null;
+        this.ethereumHighPrice = null;
+        this.ethereumLowPrice = null;
         this.state = {
-            apiData:[], 
-            btc:{argVal: null, low: null, high: null}, 
-            eth:{argVal: null, low: null, high: null}
+            apiData:[],
+            done: false, 
+            display: <div />,
+            btc:{argVal: null, low: null, high: null, LP: null, HP: null}, 
+            eth:{argVal: null, low: null, high: null, LP: null, HP: null}
         }
     }
 
     findSmallestBid = (array, signal) => {
+        console.log(array);
         let smallestBidObject = {smallestBid : null, exchange : null, name: null};
         array.forEach((trade) => {
             if(trade.name === signal){
@@ -47,7 +54,7 @@ class Indicators extends React.Component {
                 }
             }
         });
-        console.log("small ==>" + smallestBidObject.smallestBid);
+        console.log(smallestBidObject.exchange + " " + smallestBidObject.smallestBid);
         return smallestBidObject;
     }
     
@@ -63,13 +70,14 @@ class Indicators extends React.Component {
                     exchange: trade.exchange,
                     name: trade.name
                 };
-            } else if (largestBidObject.largestBid > trade.bid) {
+            } 
+            if (largestBidObject.largestBid < trade.bid) {
                 largestBidObject.largestBid = trade.bid;
                 largestBidObject.exchange = trade.exchange;
                 largestBidObject.name = trade.name;
             }}
         });
-        console.log("Large ==>" + largestBidObject.largestBid);
+        console.log(largestBidObject.exchange + " " + largestBidObject.largestBid);
         return largestBidObject;
     }
     
@@ -78,73 +86,100 @@ class Indicators extends React.Component {
         let largestAskObject = {largestAsk: null, exchange : null, name: null};
     
         array.forEach((trade) => {
-            if (largestAskObject.largestAsk === null) {
-                largestAskObject = {
-                    largestAsk : trade.ask,
-                    exchange : trade.exchange,
-                    name: trade.name
-                };
-            } else if (largestAskObject.largestAsk < trade.ask) {
-                largestAskObject.largestAsk = trade.ask;
-                largestAskObject.exchange = trade.exchange;
-                largestAskObject.name = trade.name;
+            if(trade.name === signal){
+                if (largestAskObject.largestAsk === null) {
+                    largestAskObject = {
+                        largestAsk : trade.ask,
+                        exchange : trade.exchange,
+                        name: trade.name
+                    };
+                } else if (largestAskObject.largestAsk < trade.ask) {
+                    largestAskObject.largestAsk = trade.ask;
+                    largestAskObject.exchange = trade.exchange;
+                    largestAskObject.name = trade.name;
+                }
             }
         });
         return  largestAskObject;
     }
+    // find Smallest ask
+    findSmallestAsk = (array, signal) => {
+        let smallestAskObject = {smallestAsk: null, exchange : null, name: null};
+        array.forEach((trade) => {
+            if(trade.name === signal){
+                if (smallestAskObject.smallestAsk === null) {
+                    smallestAskObject = {
+                        smallestAsk : trade.ask,
+                        exchange : trade.exchange,
+                        name: trade.name
+                    };
+                } 
+                if (smallestAskObject.smallestAsk > trade.ask) {
+                    smallestAskObject.smallestAsk = trade.ask;
+                    smallestAskObject.exchange = trade.exchange;
+                    smallestAskObject.name = trade.name;
+                }
+            }
+        });
+        return  smallestAskObject;
+    }
     
     //function to determine if arbitrage is available
-    percentageOfArbitrageAvailable = (bid, ask) => {
-        return (bid / ask) * 100;
+    percentageOfArbitrageAvailable = (diff, high) => {
+        return (diff * 100) / high;
     }
 
     componentDidMount(){   
         setInterval(() => {
             axios.get(`${server}/get-producttickergdax`)
-            .then((res) => {
-                res.data.bid = Number(res.data.bid);
-                res.data.ask = Number(res.data.ask);
-                this.setState((props) => {
-                    props.apiData.push(res.data)
-                });
-            });
+            .then((res1) => {
+                res1.data.bid = Number(res1.data.bid);
+                res1.data.ask = Number(res1.data.ask);
+                axios.get(`${server}/get-geminiBTC`)
+                .then((res2) => {
+                    res2.data.bid = Number(res2.data.bid);
+                    res2.data.ask = Number(res2.data.ask);
+                    axios.get(`${server}/get-geminiETH`)
+                    .then((res3) => {
+                        res3.data.bid = Number(res3.data.bid);
+                        res3.data.ask = Number(res3.data.ask);
+                        axios.get(`${server}/get-poloniexBTC`)
+                        .then((res4) => {
+                            res4.data.bid = Number(res4.data.bid);
+                            res4.data.ask = Number(res4.data.ask);
+                            axios.get(`${server}/get-poloniexETH`)
+                            .then((res5) => {
+                                res5.data.bid = Number(res5.data.bid);
+                                res5.data.ask = Number(res5.data.ask);
+                                this.setState((props) => {
+                                    props.apiData.push(res1.data);
+                                    props.apiData.push(res2.data);
+                                    props.apiData.push(res3.data);
+                                    props.apiData.push(res4.data);
+                                    props.apiData.push(res5.data);
+                                });
+                            })
+                            .catch(err => console.log(err));
+                        })
+                        .catch(err => console.log(err));
+                    })
+                    .catch(err => console.log(err));
+                })
+                .catch(err => console.log(err));
+            })
+            .catch(err => console.log(err));
 
-            axios.get(`${server}/get-geminiBTC`)
-            .then((res) => {
-                res.data.bid = Number(res.data.bid);
-                res.data.ask = Number(res.data.ask);
-                this.setState((props) => {
-                    props.apiData.push(res.data)
-                });
-            });
+            
 
-            axios.get(`${server}/get-geminiETH`)
-            .then((res) => {
-                res.data.bid = Number(res.data.bid);
-                res.data.ask = Number(res.data.ask);
-                this.setState((props) => {
-                    props.apiData.push(res.data)
-                });
-            });
-            axios.get(`${server}/get-poloniexBTC`)
-            .then((res) => {
-                res.data.bid = Number(res.data.bid);
-                res.data.ask = Number(res.data.ask);
-                this.setState((props) => {
-                    props.apiData.push(res.data)
-                });
-            });
-            axios.get(`${server}/get-poloniexETH`)
-            .then((res) => {
-                res.data.bid = Number(res.data.bid);
-                res.data.ask = Number(res.data.ask);
-                this.setState((props) => {
-                    props.apiData.push(res.data);
-                });
-            });
+            
+            
+            
 
-            console.log(this.findSmallestBid(this.state.apiData, "BTC_USD"));
-            console.log(this.findLargestBid(this.state.apiData, "BTC_USD"));
+            if(this.state.apiData.length === 5) this.setState({done: true});
+            if(this.state.done){
+
+            console.log(this.findSmallestAsk(this.state.apiData, "BTC_USD"));
+            console.log(this.findLargestAsk(this.state.apiData, "BTC_USD"));
 
 
 
@@ -160,41 +195,69 @@ class Indicators extends React.Component {
             const bitcoinLargestAskPrice = bitcoinLargestAskObject.largestAsk;
             const bitcoinLargestAskExchange = bitcoinLargestAskObject.exchange;
 
-            const percentageOfBitcoinArbitrageProfitable = Math.floor(this.percentageOfArbitrageAvailable(bitcoinSmallestBidPrice, bitcoinLargestAskPrice));
+            const bitcoinSmallestAskObject = this.findSmallestAsk(this.state.apiData, "BTC_USD");
+            const bitcoinSmallestAskPrice = bitcoinSmallestAskObject.smallestAsk;
+            const bitcoinSmallestAskExchange = bitcoinSmallestAskObject.exchange;
+
+            const percentageOfBitcoinArbitrageProfitable = this.percentageOfArbitrageAvailable(this.bitcoinHighPrice - this.bitcoinLowPrice, bitcoinLargestAskPrice);
 
             const ethereumSmallestBidObject = this.findSmallestBid(this.state.apiData, "ETH_USD");
             const ethereumSmallestBidPrice = ethereumSmallestBidObject.smallestBid;
             const ethereumSmallestBidExchange = ethereumSmallestBidObject.exchange;
 
             const ethereumLargestBidObject = this.findLargestBid(this.state.apiData, "ETH_USD");
-            const ethereumLargestBidPrice = ethereumLargestBidObject.smallestBid;
+            const ethereumLargestBidPrice = ethereumLargestBidObject.largestBid;
             const ethereumLargestBidExchange = ethereumLargestBidObject.exchange;
 
             const ethereumLargestAskObject = this.findLargestAsk(this.state.apiData, "ETH_USD");
             const ethereumLargestAskPrice = ethereumLargestAskObject.largestAsk;
             const ethereumLargestAskExchange = ethereumLargestAskObject.exchange;
 
-            const percentageOfEthereumArbitrageProfitable = Math.floor(this.percentageOfArbitrageAvailable(ethereumSmallestBidPrice, ethereumLargestAskPrice));
+            const ethereumSmallestAskObject = this.findSmallestAsk(this.state.apiData, "ETH_USD");
+            const ethereumSmallestAskPrice = ethereumSmallestAskObject.smallestAsk;
+            const ethereumSmallestAskExchange = ethereumSmallestAskObject.exchange;
+
+            const percentageOfEthereumArbitrageProfitable = this.percentageOfArbitrageAvailable(this.ethereumHighPrice - this.ethereumLowPrice, ethereumLargestAskPrice);
 
             this.bitcoinArbitrageValue = percentageOfBitcoinArbitrageProfitable;
-            this.bitcoinLowSeller = bitcoinSmallestBidExchange ;
+            this.bitcoinLowSeller = bitcoinSmallestAskExchange ;
             this.bitcoinHighBuyer = bitcoinLargestAskExchange ;
+            this.bitcoinHighPrice = bitcoinLargestAskPrice;
+            this.bitcoinLowPrice = bitcoinSmallestAskPrice;
             this.ethereumArbitrageValue = percentageOfEthereumArbitrageProfitable ;
-            this.ethereumLowSeller = ethereumSmallestBidExchange ;
+            this.ethereumLowSeller = ethereumSmallestAskExchange ;
             this.ethereumHighBuyer = ethereumLargestAskExchange ;
+            this.ethereumHighPrice = ethereumLargestAskPrice;
+            this.ethereumLowPrice = ethereumSmallestAskPrice;
             this.setState({
                 btc:{
                     argVal: this.bitcoinArbitrageValue,
                     low:this.bitcoinLowSeller,
-                    high:this.bitcoinLowSeller
+                    high:this.bitcoinLowSeller,
+                    LP:this.bitcoinLowPrice,
+                    HP:this.bitcoinHighPrice
                 },
                 eth: {
-                    argVal:this.ethereumArbitrageValue,
+                    argVal: this.ethereumArbitrageValue,
                     low:this.ethereumLowSeller,
-                    high:this.ethereumHighBuyer
+                    high:this.ethereumHighBuyer,
+                    LP:this.ethereumLowPrice,
+                    HP:this.ethereumHighPrice   
                 }
             });
-            this.setState({apiData: []});
+            this.setState({
+                apiData: [],
+                done: false,
+                display: <div className="container">
+                <label className="icons">Largest Possible BitCoin Arbitrage Percent <br/><br/>
+                    <DonutChart value={this.state.btc.argVal} lowSeller={this.state.btc.low} highBuyer={this.state.btc.high} highPrice={this.state.btc.HP} lowPrice={this.state.btc.LP}/>
+                </label>
+                <label className= "icons">Largest Possible Ethereum Arbitrage Percent <br/><br/>
+                    <DonutChart value={this.state.eth.argVal} lowSeller={this.state.eth.low} highBuyer={this.state.eth.high} highPrice={this.state.eth.HP} lowPrice={this.state.eth.LP}/>
+                </label>
+            </div>
+            });
+        }
         }, 5000);
     }
 
@@ -202,14 +265,7 @@ class Indicators extends React.Component {
         return (
             <div>
                 <BrowserRouter><Link to="/historicalData" className="link">See Historical Data</Link></BrowserRouter>
-                <div className="container">
-                    <label className="icons">Largest Possible BitCoin Arbitrage Percent <br/><br/>
-                        <DonutChart value={this.state.btc.argVal} lowSeller={this.state.btc.low} highBuyer={this.state.btc.high} />
-                    </label>
-                    <label className= "icons">Largest Possible Ethereum Arbitrage Percent <br/><br/>
-                        <DonutChart value={this.state.eth.argVal} lowSeller={this.state.eth.low} highBuyer={this.state.eth.high} />
-                    </label>
-                </div>
+                {this.state.display}
             </div>
         );
     }
